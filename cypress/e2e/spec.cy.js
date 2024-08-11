060e1a13f63c969670039046fa945470906ab9cc
kpetrauskaite57 <kpetrauskaite57@gmail.com>
2024-08-11 22:11:55 +0300

    111

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,32 +4,47 @@ describe('Shopping Cart Functionality', () => {
     cy.visit('https://qa-practice.netlify.app/products_list');
   });
 
+  it('Displays the shopping cart', () => {
+    cy.get("section-header").should("be.visible");
+});
+
+it('Displays the purchase button', () => {
+  cy.get("btn.btn-primary.btn-purchase").should("be.visible");
+});
+
+it('Displays the product list', () => {
+  cy.get('.shop-item-title').should('be.visible');
+});
+
+  it('Displays the product price', () => {
+    cy.get('.shop-item-price').should('be.visible');
+  });
+
+  it('Displays the add to cart button', () => {
+    cy.get('.shop-item-button').should('be.visible');
+  });
+
   it('should add a product to the cart', () => {
-    cy.get('.product').first().find('.btn.btn-primary.shop-item-button').click();
-    cy.get('.cart-items').should('contain', '1 item');
+    cy.get('.shop-item-title').first().find('.shop-item-button').click();
+    cy.get('.cart-total-price').should('contain', '$236.12');
   });
 
   it('should remove a product from the cart', () => {
-    cy.get('.product').first().find('.btn.btn-primary.shop-item-button').click();
-    cy.get('.cart').find('.btn.btn-danger').click();
-    cy.get('.cart-items').should('contain', '0 items');
+    cy.get('.cart-items').first().find('.btn-danger').click();
+    cy.get('.cart-total-price').should('contain', '$0');
   });
 
   it('should update product quantity in the cart', () => {
-    cy.get('.product').first().find('.btn.btn-primary.shop-item-button').click();
-    cy.get('.cart').find('.quantity-input').clear().type('2');
-    cy.get('.cart-total-price').should('contain', 'Total: $1811.98');
+    cy.get('.shop-item-title').first().find('.shop-item-button').click();
+    cy.get('.cart-total-price').should('contain', '$236.12');
+    cy.get('.cart-quantity-input').first().clear().type('2');
+    cy.get('.cart-total-price').should('contain', '$472.24');
   });
 
-  it('should remove cart contents after page refresh', () => {
-    cy.get('.product').first().find('.add-to-cart-btn').click();
-    cy.reload();
-    cy.get('.cart-total-price').should('contain', '$0');
-  });
 
   it('should initiate the checkout process', () => {
     cy.get('.product').first().find('.btn.btn-primary.shop-item-button').click();
     cy.get('.section-header').find('.btn.btn-primary.btn-purchase').click();
-    cy.url(https://qa-practice.netlify.app/products_list).should('include', 'Congrats! Your order of $905.99 has been registered!');
+    cy.url('https://qa-practice.netlify.app/products_list').should('include', 'Congrats! Your order of $905.99 has been registered!');
   });
 });
